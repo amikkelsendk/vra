428d6719f780fb155804797e8023b08caef8db8e
amikkelsendk <am@amikkelsen.com>
2024-10-30 13:47:06 +0100

    Initial

---
+++ retrieve_all_deployments_in_chunks_of_x-REST.ps1
@@ -0,0 +1,124 @@
+# https://www.vcrocs.info/rest-api-calls-vmware-aria-automation/
+
+Function Get-vRARefreshToken {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$vraServer,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]$Credential 
+    )
+
+    # Get Credentials to build auth:
+    $username = $Credential.UserName
+    $pass = $Credential.Password
+    $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass))
+    $authBody  = @{
+        "username" = $username
+        "password" = $password
+    }
+    
+    # Build Header:
+    $header = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $header.Add("Accept","application/json")
+    $header.Add("Content-Type","application/json")
+    
+    # Get Refresh Token:
+    $uri = "https://" + $vraServer + "/csp/gateway/am/api/login?access_token"
+    $thisRefreshToken = Invoke-RestMethod -uri $uri -Method POST -Headers $header -Body ($authBody | ConvertTo-Json) #-SkipCertificateCheck
+    
+    Return $thisRefreshToken.refresh_token
+}
+
+Function Get-vRABearerToken {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$vraServer,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$RefreshToken
+    )
+       
+    # Build Header:
+    $header = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $header.Add("Accept","application/json")
+    $header.Add("Content-Type","application/json")
+    
+    # Build Body
+    $refreshTokenBody = @{
+        "refreshToken" = $RefreshToken
+    }
+
+    # Get Access Token:
+    $uri = "https://" + $vraServer + "/iaas/api/login"
+    $accessToken = Invoke-RestMethod -Uri $uri -Method POST -Headers $header -body ($refreshTokenBody | ConvertTo-JSON) #-SkipCertificateCheck
+    $accessToken = "Bearer " + $accessToken.token
+    
+    Return $accessToken
+}
+
+
+####################################################
+
+
+# BasicInfo:
+$vraServer = "< vra server FQDN or IP"
+
+# Get vRA Credentials
+$Credential = Get-Credential -Message "Credentials for vRA Server"
+
+$refreshToken = Get-vRARefreshToken -vraServer $vraServer -Credential $Credential
+$accessToken = Get-vRABearerToken -vraServer $vraServer -RefreshToken $refreshToken
+
+# Build Header:
+$header = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$header.Add("Accept","application/json")
+$header.Add("Content-Type","application/json")
+$header.Add("Authorization",$accessToken)       # add access token to Header
+
+
+# Query API
+$top = 20
+$found = 0
+$arrDeployments = @()
+
+do {
+    Write-Host "Found: $found"
+    $uri = "https://" + $vraServer + '/deployment/api/deployments?$top=' + $top + '&$skip=' + $found
+    $result = Invoke-WebRequest -uri $uri -Method GET -Headers $header 
+
+    if ( $result.StatusCode -eq 200 ) {
+        $resultJSON = ( $result | ConvertFrom-Json )
+        if ( $resultJSON.numberOfElements -gt 0) {
+            $arrDeployments += $resultJSON.Content
+        }
+    }
+    $found += $resultJSON.numberOfElements
+}
+while ( $resultJSON.totalElements -gt $found )
+
+$arrDeployments.count
+
+
+
+###################   EXTRA #####################
+#For PS5.1 fixing error
+# Invoke-RestMethod : The underlying connection was closed: Could not establish trust relationship for
+# the SSL/TLS secure channel.
+<#
+$code= @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy {
+            public bool CheckValidationResult(ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem) {
+                return true;
+            }
+        }
+"@
+Add-Type -TypeDefinition $code -Language CSharp
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+#>
